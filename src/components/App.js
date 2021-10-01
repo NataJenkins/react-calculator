ffdaf776ca6ad888478e551e190b8eebb4dbf175
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-09-30 22:39:54 -0500

    app moved to components folder

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ButtonPanel from "./ButtonPanel";
+import Display from "./display";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Project Calculator</h1>
+
+      <div className="calculator">
+        <Display result={0} />
+        <ButtonPanel />
+      </div>
+    </div>
+  );
+}
+export default App;
