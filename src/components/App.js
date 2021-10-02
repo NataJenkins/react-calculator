b2c7b406619a2e2d2f78f9cf3428fd01e0775b25
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-10-02 16:16:11 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ButtonPanel from './buttonPanel';
 import Display from './display';
+import calculate from '../logic/calculate';
 
 function App() {
   return (
     <>
       <h1 className="title">Project Calculator</h1>
-
       <div className="calculator">
-        <Display result={0} />
+        <Display result={calculate} />
         <ButtonPanel />
       </div>
     </>
